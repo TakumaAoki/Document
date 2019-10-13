03be0f7b0daea7cac9e8afe798038d07dc8510ed
Takuma Aoki <taku1106aoki@gmail.com>
2019-10-13 11:21:59 +0900

    document 2

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+    validates :age, numericality: { greater_than_or_equal_to: 0 }
+    has_many :posts, dependent: :destroy
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190926031609) do
+ActiveRecord::Schema.define(version: 20191012075327) do
+
+  create_table "posts", force: :cascade do |t|
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -18,6 +26,8 @@ ActiveRecord::Schema.define(version: 20190926031609) do
     t.integer "age"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "country"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
